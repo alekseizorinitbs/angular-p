62259eb1d2603b606a463b5ead13096abc2d1a1f
alekseizorinitbs <alexey.zorin.itbs@gmail.com>
2017-06-04 21:33:02 +0300

    case service dehardcoded

--- app/services/case.service.ts
+++ app/services/case.service.ts
@@ -13,7 +13,7 @@ private _url = "https://localhost:8443/prweb/api/v1/";
 private _urlExtra = "https://localhost:8443/prweb/PRRestService/GPNDataManager/GPNDataManager/";
 
   parentCase = new Subject<GPNRequest>();
-  headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' });
+  headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':this._cacheService.get('creds')});
   options = new RequestOptions({ headers: this.headers });
 
   parentCase$ = this.parentCase.asObservable();
@@ -21,7 +21,7 @@ private _urlExtra = "https://localhost:8443/prweb/PRRestService/GPNDataManager/G
   constructor(private _http: Http, private _authGuard: AuthenticatedGuard, private _cacheService: CacheService){
     this._authGuard.authenticates$.subscribe(result => {
       this.headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':this._cacheService.get('creds')});
-      console.log('new creds:' + this._cacheService.get('creds'));
+      this.options = new RequestOptions({ headers: this.headers });
     });
   }
 
@@ -42,38 +42,29 @@ private _urlExtra = "https://localhost:8443/prweb/PRRestService/GPNDataManager/G
 
   getCases(){
 
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' });
-    let options = new RequestOptions({ headers: headers });
-
-    return this._http.get(this._url + 'cases/', options)
+    return this._http.get(this._url + 'cases/', this.options)
     .map(data => data.json());
   }
 
   getCase(id){
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' });
-    let options = new RequestOptions({ headers: headers });
-    console.log("rest called");
-    return this._http.get(this._url + 'cases/' + id, options)
+
+    return this._http.get(this._url + 'cases/' + id, this.options)
     .map(data => data.json());
   }
 
   getAttachedDocuments(caseId){
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' });
-    let options = new RequestOptions({ headers: headers });
-    return this._http.get('https://localhost:8443/prweb/PRRestService/GPNDataManager/GPNDataManager/DocumentManager/' + caseId, options)
+    return this._http.get('https://localhost:8443/prweb/PRRestService/GPNDataManager/GPNDataManager/DocumentManager/' + caseId, this.options)
     .map(data => data.json());
   }
 
   createCase(_case: GPNRequest) : Observable<any> {
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' });
-    let options = new RequestOptions({ headers: headers });
 
-    return this._http.post(this._url + 'cases/', JSON.stringify(_case),options)
+    return this._http.post(this._url + 'cases/', JSON.stringify(_case), this.options)
     .map(data => data.json());
   }
 
   getDocument(docid){
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz', 'docid':docid ,
+    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':this._cacheService.get('creds'), 'docid':docid ,
     'responseContentType':'application/octet-stream' , 'content-disposition':'Content-Disposition: inline; filename="????????.docx"'});
     let options = new RequestOptions({ headers: headers });
 
@@ -89,7 +80,7 @@ private _urlExtra = "https://localhost:8443/prweb/PRRestService/GPNDataManager/G
     etag = '"' + etag.replace(/-/g,'').replace(/Z/g, ' GMT').replace(/:/g,'') + '"';
     console.log(etag);
 
-    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':'Basic QWRtaW5AQW5ndWxhclA6MTIz' , 'If-Match' : etag});
+    let headers = new Headers({ 'Content-Type': 'application/json', 'Authorization':this._cacheService.get('creds'), 'If-Match' : etag});
     let options = new RequestOptions({ headers: headers });
 
     return this._http.put(this._url + 'cases/' + _case.ID + '/?actionID=' + actionId, _case, options)
